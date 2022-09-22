3503189dc72bb9a790c8485da58ecafc907b7b46
Dennis Akagha <d.akagha20@gmail.com>
2022-09-22 22:07:46 +0100

    Created dummy data, add template and static files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+posts = [
+    {
+        'author': 'Dennis Akagha',
+        'title': 'This is for developers',
+        'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium omnis odio adipisci numquam harum necessitatibus, voluptatum vel veniam tempora assumenda.',
+        'dateCreated': '12th, March 2020',
+    },
+     {
+        'author': 'Peter Joel',
+        'title': 'Learn Django in three days',
+        'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium omnis odio adipisci numquam harum necessitatibus, voluptatum vel veniam tempora assumenda.',
+        'dateCreated': '15th, March 2022',
+    },
+     {
+        'author': 'Okechukw Daniel',
+        'title': 'Learn HTML',
+        'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium omnis odio adipisci numquam harum necessitatibus, voluptatum vel veniam tempora assumenda.',
+        'dateCreated': '15th, March 2022',
+    }
+]
+
+
+
+def home(request):
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context)
