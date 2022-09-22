407b636dea7bf571a67e0a11f43af9a872d2f34a
Dennis Akagha <d.akagha20@gmail.com>
2022-09-22 23:13:44 +0100

    Add a Post model

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,9 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'Dennis Akagha',
-        'title': 'This is for developers',
-        'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium omnis odio adipisci numquam harum necessitatibus, voluptatum vel veniam tempora assumenda.',
-        'dateCreated': '12th, March 2020',
-    },
-     {
-        'author': 'Peter Joel',
-        'title': 'Learn Django in three days',
-        'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium omnis odio adipisci numquam harum necessitatibus, voluptatum vel veniam tempora assumenda.',
-        'dateCreated': '15th, March 2022',
-    },
-     {
-        'author': 'Okechukw Daniel',
-        'title': 'Learn HTML',
-        'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium omnis odio adipisci numquam harum necessitatibus, voluptatum vel veniam tempora assumenda.',
-        'dateCreated': '15th, March 2022',
-    }
-]
+from .models import Post
 
 
 
 def home(request):
+    posts = Post.objects.all()
     context = {'posts': posts}
     return render(request, 'blog/home.html', context)
