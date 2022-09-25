7664838feda154fe81cb9fd0972a205f79a6f5f3
Dennis Akagha <d.akagha20@gmail.com>
2022-09-25 02:40:33 +0100

    Add image upload to profile user

--- blogproj/urls.py
+++ blogproj/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+
 from django.contrib.auth import views as auth_view # we are using this to access the in-built django LoginView and LogoutView
 
 urlpatterns = [
@@ -24,5 +30,12 @@ urlpatterns = [
 
     path('register/', user_views.register, name='register'),
     path("login/", auth_view.LoginView.as_view(template_name='users/login.html'), name="login"),
-    path("logout/", auth_view.LogoutView.as_view(template_name='users/logout.html'), name="logout")
+    path("logout/", auth_view.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+    path("profile/", user_views.profile, name='profile'),
+    path("profile_update/", user_views.profile_update, name='profile_update')
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
